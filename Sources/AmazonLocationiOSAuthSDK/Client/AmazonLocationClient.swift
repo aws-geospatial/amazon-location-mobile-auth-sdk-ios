ccbce1708c82e29d9d7f35f23bfabf9c9256c73c
Zeeshan Sheikh <64203935+zeeshanmakeen@users.noreply.github.com>
2024-05-23 18:03:54 +0500

    Update AmazonLocationClient.swift

--- Sources/AmazonLocationiOSAuthSDK/Client/AmazonLocationClient.swift
+++ Sources/AmazonLocationiOSAuthSDK/Client/AmazonLocationClient.swift
@@ -7,7 +7,7 @@ public class AmazonLocationClient {
         self.locationProvider = locationCredentialsProvider
     }
     
-    public func sendRequest<T: Decodable, E: AmazonErrorResponse>(
+    public func sendRequest<T: Decodable, E: AmazonErrorResponse & Error>(
         serviceName: AmazonService,
         endpoint: AmazonLocationEndpoint,
         httpMethod: HTTPMethod,
@@ -16,38 +16,39 @@ public class AmazonLocationClient {
         errorType: E.Type
     ) async throws -> Result<T, E> {
         let url = URL(string: endpoint.url())!
-        
+
         var request = URLRequest(url: url)
         request.httpMethod = httpMethod.rawValue
         var requestData: Data? = nil
-        
+
         if let requestBody = requestBody {
             do {
                 requestData = try requestBody.toData()
                 request.httpBody = requestData
             } catch {
+                print("Error: Unable to encode request body as JSON")
                 throw error
             }
         }
-        
+
         var headers = HTTPHeaders()
         headers.add(name: "Content-Type", value: "application/json")
-        
+
         let cognitoCredentials = locationProvider.getCognitoProvider()?.getCognitoCredentials()
         let signer = AWSSigner(credentials: cognitoCredentials!, serviceName: serviceName.rawValue, region: locationProvider.getRegion()!)
         let signedHeaders = signer.signHeaders(url: url, method: httpMethod, headers: headers, body: requestData != nil ? .data(requestData!) : nil)
-        
+
         for (name, value) in signedHeaders.allHeaders() {
             request.addValue(value, forHTTPHeaderField: name)
         }
-        
+
         do {
             let (data, response) = try await URLSession.shared.data(for: request)
-            
+
             guard let httpResponse = response as? HTTPURLResponse else {
                 throw URLError(.badServerResponse)
             }
-            
+
             if (200...299).contains(httpResponse.statusCode) {
                 let decoder = JSONDecoder()
                 let successResponse = try decoder.decode(successType, from: data)
@@ -58,9 +59,11 @@ public class AmazonLocationClient {
                 return .failure(errorResponse)
             }
         } catch {
+            print("Error: \(error.localizedDescription)")
             throw error
         }
     }
+
 
     public func searchPosition(indexName: String, request: SearchByPositionRequest) async throws -> SearchByPositionResponse? {
         
