ef36887e6fffb974e87c428eca9525b759b3689b
Zeeshan Sheikh <64203935+zeeshanmakeen@users.noreply.github.com>
2024-05-29 14:43:34 +0500

    Update SearchByPositionRequest.swift

--- Sources/AmazonLocationiOSAuthSDK/Types/ApiTypes/RequestTypes/SearchByPositionRequest.swift
+++ Sources/AmazonLocationiOSAuthSDK/Types/ApiTypes/RequestTypes/SearchByPositionRequest.swift
@@ -10,6 +10,12 @@ public struct SearchByPositionRequest: Codable, EncodableRequest {
         case maxResults = "MaxResults"
         case position = "Position"
     }
+    
+    public init(language: String, maxResults: Int, position: [Double]) {
+        self.language = language
+        self.maxResults = maxResults
+        self.position = position
+    }
 
     public func toData() throws -> Data {
         let encoder = JSONEncoder()
