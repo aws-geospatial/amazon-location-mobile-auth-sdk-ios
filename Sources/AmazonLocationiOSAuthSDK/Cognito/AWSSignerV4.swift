3dbac0be4d38bc0f311f82f156ef280beb1349ba
Zeeshan Sheikh <64203935+zeeshanmakeen@users.noreply.github.com>
2024-06-12 23:57:21 +0500

    Update AWSSignerV4.swift

--- Sources/AmazonLocationiOSAuthSDK/Cognito/AWSSignerV4.swift
+++ Sources/AmazonLocationiOSAuthSDK/Cognito/AWSSignerV4.swift
@@ -20,7 +20,7 @@ public class AWSSignerV4 {
             let resolver:AWSClientRuntime.StaticAWSCredentialIdentityResolver? =  try StaticAWSCredentialIdentityResolver(AWSCredentialIdentity(accessKey: credentials.accessKeyId, secret: credentials.secretKey, expiration: credentials.expiration, sessionToken: credentials.sessionToken))
             
             var headers = Headers()
-            headers.add(name: "host", value: Self.hostname(from: url))
+            headers.add(name: "host", value: self.hostname(from: url))
             let requestBuilder = SdkHttpRequestBuilder().withHost(url.host!).withPath(url.path)
                 .withHeaders(headers)
             
@@ -30,11 +30,11 @@ public class AWSSignerV4 {
         return nil
     }
     
-    private static func hostname(from url: URL) -> String {
+    private func hostname(from url: URL) -> String {
         "\(url.host ?? "")\(self.port(from: url).map { ":\($0)" } ?? "")"
     }
     
-    private static func port(from url: URL) -> Int? {
+    private func port(from url: URL) -> Int? {
         guard let port = url.port else { return nil }
         guard url.scheme != "http" || port != 80 else { return nil }
         guard url.scheme != "https" || port != 443 else { return nil }
